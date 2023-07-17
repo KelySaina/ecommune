1ba9aada450ca6468d4b39b8e4ac844d21cfb7ab
KelySaina <kelysaina@gmail.com>
2023-07-17 14:49:02 +0300

    ks.app

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import Login from "./pages/Login"
 import Acceuil from "./pages/Acceuil"
-import ListProjet from './components/ListProjet'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
@@ -15,10 +14,9 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="ListProjet">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Acceuil" component={Acceuil} />
-          <Stack.Screen name="ListProjet" component={ListProjet} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
